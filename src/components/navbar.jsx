4d6f422b71256aacb60e14f8125b9e267c296fab
zample23 <egor33parasiuk@gmail.com>
2024-03-29 16:45:18 +0200

    Header styles changed

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -6,10 +6,12 @@ import './navbar.scss';
 export const Navbar = () => {
   return (
     <div className="navbar">
-      <div className="navbar__links">
+      <div className="navbar__link">
         <Link to="/">Shop</Link>
+      </div>
+      <div className="navbar__link">
         <Link to="/cart">
-          <ShoppingCart size={32} />
+          <ShoppingCart size={40} />
         </Link>
       </div>
     </div>
